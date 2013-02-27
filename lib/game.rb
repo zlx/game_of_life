f4f969a6fa4f93480c1ef433685be6ad905a2738
soffolk <zlx.star@gmail.com>
2013-02-27 22:08:05 +0800

    complete?

--- lib/game.rb
+++ lib/game.rb
@@ -10,7 +10,11 @@ class GameOfLife
 
     @y_length.times.each_with_index do |j|
       @x_length.times.each_with_index do |i|
-        if alive?(i, j) && has_2_neighbor_alive?(i, j)
+        if alive?(i, j) && has_2_or_3_neighbor_alive?(i, j)
+          set_alive(i, j)
+        end
+
+        if !alive?(i, j) && has_3_neighbor_alive?(i, j)
           set_alive(i, j)
         end
       end
@@ -25,14 +29,27 @@ class GameOfLife
     @init_grid[j][i] == 1
   end
 
-  def has_2_neighbor_alive?(i, j)
+  def has_2_or_3_neighbor_alive?(i, j)
+    count = neighbor_alive_count(i, j)
+    2 == count || 3 == count
+  end
+
+  def has_3_neighbor_alive?(i, j)
+    3 == neighbor_alive_count(i, j)
+  end
+
+  def neighbor_alive_count(i, j)
     count = 0
     count += 1 if up_alive?(i, j)
     count += 1 if down_alive?(i, j)
     count += 1 if left_alive?(i, j)
     count += 1 if right_alive?(i, j)
+    count += 1 if left_down_alive?(i, j)
+    count += 1 if right_up_alive?(i, j)
+    count += 1 if right_down_alive?(i, j)
+    count += 1 if left_up_alive?(i, j)
 
-    count == 2
+    count
   end
 
   def set_alive(i, j)
@@ -53,5 +70,21 @@ class GameOfLife
 
   def right_alive?(i, j)
     i < @x_length && @init_grid[j][i+1] == 1
+  end
+
+  def left_down_alive?(i, j)
+    i > 0 && j < @y_length-1 && @init_grid[j+1][i-1] == 1
+  end
+
+  def right_up_alive?(i, j)
+    i < @x_length-1 && j > 0 && @init_grid[j-1][i+1] == 1
+  end
+
+  def right_down_alive?(i, j)
+    i < @x_length-1 && j < @y_length-1 && @init_grid[j+1][i+1] == 1
+  end
+
+  def left_up_alive?(i, j)
+    i > 0 && j > 0 && @init_grid[j-1][i-1] == 1
   end
 end
