6948f9803a1b0cdcced3a0a78a413427cec6f500
soffolk <zlx.star@gmail.com>
2013-02-28 23:29:54 +0800

    remove better errors

--- demo.rb
+++ demo.rb
@@ -1,12 +1,5 @@
 require 'rubygems'
 require 'sinatra'
-require "better_errors"
-
-configure :development do
-  use BetterErrors::Middleware
-  BetterErrors.application_root = File.expand_path("..", __FILE__)
-end
-
 require './lib/game'
 
 get '/' do
